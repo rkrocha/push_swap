97cf1d684574e1a4821321e5bcff49bb37d46e5d
rkochhan <rkochhan@student.42sp.org.br>
2021-09-14 10:28:02 -0300

    added parsing and utils

---
+++ parse_args.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_args.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/13 15:38:42 by rkochhan          #+#    #+#             */
+/*   Updated: 2021/09/14 10:21:19 by rkochhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "push_swap.h"
+#include <limits.h>
+
+static bool	num_isrepeat(t_stack *stack, long long num)
+{
+	t_dlist	*tracker;
+
+	tracker = stack->top;
+	while (tracker)
+	{
+		if (num == tracker->content)
+			return (true);
+		tracker = tracker->next;
+	}
+	return (false);
+}
+
+static bool	num_isvalid(long long num)
+{
+	if (num > INT_MAX || num < INT_MIN)
+		return (false);
+	return (true);
+}
+
+static bool	str_isvalid(const char *str)
+{
+	if (*str == '+' || *str == '-')
+		str++;
+	if (*str && !ft_strignore(str, "0123456789"))
+		return (true);
+	return (false);
+}
+
+void	parse_args(t_data *frame, int argc, const char **argv)
+{
+	long long	num;
+
+	argc--;
+	while (argc > 0)
+	{
+		num = ft_atoll(argv[argc]);
+		if (str_isvalid(argv[argc]) == false ||
+				num_isvalid(num) == false ||
+				num_isrepeat(&frame->a_stack, num) == true)
+			error_exit(frame);
+		stack_push(&frame->a_stack, dlst_new(num));
+		argc--;
+	}
+}
---
+++ utils.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/14 08:57:30 by rkochhan          #+#    #+#             */
+/*   Updated: 2021/09/14 09:30:45 by rkochhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "push_swap.h"
+
+void	free_stacks(t_data *frame)
+{
+	stack_clear(&frame->a_stack);
+	stack_clear(&frame->b_stack);
+}
+
+void	error_exit(t_data *frame)
+{
+	free_stacks(frame);
+	ft_putendl_fd("Error", 2);
+	exit(EXIT_FAILURE);
+}
