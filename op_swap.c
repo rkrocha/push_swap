178c71ef8506d3c559d1c64f3a850d5432fd4911
rkochhan <rkochhan@student.42sp.org.br>
2021-09-20 09:28:25 -0300

    fixed op_swap for stack len = 2

--- op_swap.c
+++ op_swap.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 09:23:35 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/09/17 16:16:46 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/09/19 20:26:03 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,16 @@ static void	op_swap(t_stack *stack)
 
 	if (stack->len < 2)
 		return ;
+	if (stack->len == 2)
+	{
+		stack->top = stack->bottom;
+		stack->bottom = stack->bottom->prev;
+		stack->top->next = stack->bottom;
+		stack->top->prev = NULL;
+		stack->bottom->next = NULL;
+		stack->bottom->prev = stack->top;
+		return ;
+	}
 	popped = stack->top->next;
 	stack->top->next = popped->next;
 	popped->next->prev = popped->prev;
