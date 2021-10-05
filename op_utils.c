0e8545fafe69dfc8245037802a8cc1b19753f70d
rkochhan <rkochhan@student.42sp.org.br>
2021-10-05 12:49:28 -0300

    added function op_pa_in_num_pos

--- op_utils.c
+++ op_utils.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 09:29:07 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/09/20 11:08:55 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/10/05 12:43:10 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,21 @@ void	op_nrb(int n, t_data *frame)
 		op_n(op_rb, n, frame);
 	else if (n < 0)
 		op_n(op_rrb, -n, frame);
+}
+
+void	op_pa_in_num_pos(int num_pos, t_data *frame)
+{
+	if (num_pos >= -1 && num_pos <= 1)
+	{
+		op_pa(frame);
+		if (num_pos == 1)
+			op_sa(frame);
+		else if (num_pos == -1)
+			op_rra(frame);
+	}
+	else
+	{
+		op_nra(num_pos, frame);
+		op_pa(frame);
+	}
 }
