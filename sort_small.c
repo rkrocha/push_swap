629afb680f5f385675d0988fad3d58148f1bc94b
rkochhan <rkochhan@student.42sp.org.br>
2021-10-05 12:49:42 -0300

    mostly working small sort

--- sort_small.c
+++ sort_small.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 14:22:45 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/09/17 16:37:57 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/10/05 12:42:27 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,47 @@ void	sort_two(t_data *frame)
 
 void	sort_three(t_data *frame)
 {
-	if (content(A_STACK.top) > content(A_STACK.top->next))
+	if (value(A_STACK.top) > value(A_STACK.top->next)
+		&& value(A_STACK.top->next) < value(A_STACK.bottom)
+		&& value(A_STACK.bottom) > value(A_STACK.top))
 		op_sa(frame);
-	if (content(A_STACK.bottom) < content(A_STACK.bottom->prev)
-		|| content(A_STACK.top) > content(A_STACK.bottom))
+	else if (value(A_STACK.top) > value(A_STACK.top->next)
+		&& value(A_STACK.top->next) > value(A_STACK.bottom)
+		&& value(A_STACK.bottom) < value(A_STACK.top))
+	{
+		op_sa(frame);
 		op_rra(frame);
-	if (content(A_STACK.top) > content(A_STACK.top->next))
+	}
+	else if (value(A_STACK.top) > value(A_STACK.top->next)
+		&& value(A_STACK.top->next) < value(A_STACK.bottom)
+		&& value(A_STACK.bottom) < value(A_STACK.top))
+		op_ra(frame);
+	else if (value(A_STACK.top) < value(A_STACK.top->next)
+		&& value(A_STACK.top->next) > value(A_STACK.bottom)
+		&& value(A_STACK.bottom) > value(A_STACK.top))
+	{
 		op_sa(frame);
+		op_ra(frame);
+	}
+	else
+		op_rra(frame);
+}
+
+void	sort_four_five(t_data *frame)
+{
+	int	num_pos[2];
+
+	op_pb(frame);
+	op_pb(frame);
+	if (stack_issorted(&A_STACK) == false)
+		sort_small(frame);
+	if (B_STACK.top > B_STACK.bottom)
+		op_sb(frame);
+	num_pos[0] = peek_num_position(&A_STACK, value(B_STACK.top));
+	num_pos[1] = peek_num_position(&A_STACK, value(B_STACK.bottom));
+	op_pa_in_num_pos(num_pos[0], frame);
+	op_pa_in_num_pos(num_pos[1] + num_pos[0], frame);
+	op_nra(peek_lowest_num_position(&A_STACK), frame);
 }
 
 void	sort_small(t_data *frame)
@@ -35,4 +69,6 @@ void	sort_small(t_data *frame)
 		sort_two(frame);
 	else if (A_STACK.len == 3)
 		sort_three(frame);
+	else
+		sort_four_five(frame);
 }
